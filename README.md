f0ed3c53f0b5c0bd158ca7c282602a703a178e2f
Nicholas <nicholasjoven@gmail.com>
2022-07-26 18:07:11 -0400

    plants render, deliv 1

--- README.md
+++ README.md
@@ -34,9 +34,15 @@ The base URL for your backend is: `http://localhost:6001`
 As a user:
 
 1. When the app starts, I can see all plants.
-2. I can add a new plant to the page by submitting the form.
-3. I can mark a plant as "sold out".
-4. I can search for plants by their name and see a filtered list of plants.
+
+```javascript
+// fetch images from server
+// render images in appropriate spot
+// examine data point: if array, array.map where it belongs
+```
+3. I can add a new plant to the page by submitting the form.
+4. I can mark a plant as "sold out".
+5. I can search for plants by their name and see a filtered list of plants.
 
 ### Endpoints for Core Deliverables
 
--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PlantCard from "./PlantCard";
 
+
+let PLANTS = []
+
 function PlantList() {
+  const [myPlants, setMyPlants] = useState([])
+
+  const handleAddPlants = (newPlants) => {
+    PLANTS = [...myPlants, newPlants]
+  }
+
+  const fetchPlants = () => {
+    fetch('http://localhost:6001/plants')
+    .then(req => req.json())
+    .then(res => {
+      console.log(res)
+      res.forEach((element) => {
+          PLANTS.push(element)
+      })
+      setMyPlants(PLANTS)
+    })
+  }
+
+  useEffect(fetchPlants, [])
+  
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">
+      {myPlants.map((element, index) => {
+        return (
+          <PlantCard key={index} name={element.name} image={element.image} price={element.price} />
+        )
+      })}
+    </ul>
   );
 }
 
