d9565dbdaf99b8184944507eaf294aca7f7bf1dc
Nicholas <nicholasjoven@gmail.com>
2022-07-26 20:02:23 -0400

    starting over

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-function PlantCard({ name, image, price}) {
+function PlantCard() {
   return (
     <li className="card">
-      <img src={image} alt={name} />
-      <h4>{name}</h4>
-      <p>Price: {price}</p>
+      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
+      <h4>{"plant name"}</h4>
+      <p>Price: {"plant price"}</p>
       {true ? (
         <button className="primary">In Stock</button>
       ) : (
--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,33 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import PlantCard from "./PlantCard";
 
-
 function PlantList() {
-  const [myPlants, setMyPlants] = useState([])
-
-  const fetchPlants = async () => {
-  }
-  
-  
-  
-  useEffect(() => {
-    const fetchPlants = async () => {
-      let req = await fetch('http://localhost:6001/plants')
-      let res = await req.json()
-      // console.log(res)
-      setMyPlants(res)
-    }
-    fetchPlants();
-  }, [])
-  
   return (
-    <ul className="cards">
-      {myPlants.map((element, index) => {
-        return (
-          <PlantCard key={index} name={element.name} image={element.image} price={element.price} />
-        )
-      })}
-    </ul>
+    <ul className="cards">{/* render PlantCards components in here */}</ul>
   );
 }
 
