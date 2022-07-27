5763bba15636de46d0f33ecff016395f63227de3
Nicholas <nicholasjoven@gmail.com>
2022-07-26 23:23:21 -0400

    deliveravles + bonus deliverables complete

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 
-const PlantCard = ({id, name, image, price, handleRemovePlant}) => {
+const PlantCard = ({id, name, image, price, handleRemovePlant, handleUpdatePlant}) => {
   const [isInStock, setIsInStock] = useState(true)
-  const [updatePrice, setUpdatePrice] = useState(price)
+  const [updatedPrice, setUpdatedPrice] = useState(price)
 
   const handleIsInStock = () => {
     setIsInStock((isInStock) => !isInStock)
@@ -10,9 +10,23 @@ const PlantCard = ({id, name, image, price, handleRemovePlant}) => {
 
   const handleRemoveClick = async () => {
     await fetch(`http://localhost:6001/plants/${id}`, {
-      method: "DELETE",
+      method: 'DELETE'
     })
     handleRemovePlant(id)
+  }
+
+  const handleSubmitPrice = async () => {
+    let req = await fetch(`http://localhost:6001/plants/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ price: updatedPrice })
+    })
+    .then(req => req.json())
+    .then((res => {
+      handleUpdatePlant(res)
+    }))
   }
 
   return (
@@ -25,6 +39,25 @@ const PlantCard = ({id, name, image, price, handleRemovePlant}) => {
       ) : (
         <button onClick={handleIsInStock}>Out of Stock</button>
       )}
+      <form onSubmit={(e) => {
+        e.preventDefault()
+        handleSubmitPrice()
+      }}>
+        <input 
+          type="number"
+          name="price"
+          step="0.01"
+          placeholder="Price" 
+          value={updatedPrice}
+          onChange={(e) => {
+            setUpdatedPrice(e.target.value)
+          }}
+        />
+        <input
+          type='submit'
+          value='Update Price'
+        />
+      </form>
       <button onClick={handleRemoveClick}>Delete</button>
     </li>
   );
