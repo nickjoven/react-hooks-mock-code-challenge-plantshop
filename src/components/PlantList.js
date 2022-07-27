5bf387d462193e7d29c5a92bc1e4763567fdc0e4
Nicholas <nicholasjoven@gmail.com>
2022-07-27 17:51:26 -0400

    added comments

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,26 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+// include passed prop in parameter
+const PlantList = ({plants}) => {
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">
+      {/* render PlantCards components in here 
+       we have an array. We will .map over the array and return <PlantCard */}
+       {plants.map((element, index) => {
+         const { id, name, image, price } = element
+        return (
+          <PlantCard 
+            key={index}
+            id={id}
+            name={name}
+            image={image}
+            price={price}
+          />
+        )
+       })}
+      
+    </ul>
   );
 }
 
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,35 @@
-import React from "react";
+// must import useState and useEffect
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
-function PlantPage() {
+const PlantPage =() => {
+  // states to manage go here. plants is for the data from the server, searchTerm is for a text input that will be used to filter the displayed data
+  const [plants, setPlants] = useState([])
+  const [searchTerm, setSearchTerm] = ([])
+
+  // begin with useEffect hook to load initial data
+  useEffect(() => {
+    const fetchPlants = async () => {
+      let req = await fetch('http://localhost:6001/plants')
+      let res = await req.json()
+      setPlants(res)
+      // console.log('plants', res) // confirm data in console
+    }
+    fetchPlants()
+  }, [])
+
+  // For deliverable 1, we need the data to render. The desintation for the data is PlantCard, which is a child
+  // of PlantList. The reason we don't fetch data in PlantList is because we have other components that need
+  // to update the data, and PlantPage is the lowest common ancestor of these components.
+  // Pass the data as a prop to PlantList. * JUMP TO PLANTLIST *
+
   return (
     <main>
       <NewPlantForm />
       <Search />
-      <PlantList />
+      <PlantList plants={plants} />
     </main>
   );
 }
